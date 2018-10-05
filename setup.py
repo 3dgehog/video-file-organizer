4125767ae3b24b15e386e143fd3596fdb98bb03f
Scheercuzy <maxi730@gmail.com>
2018-10-05 13:38:07 +0630

    Inital Commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='Video File Organizer',
+    version='0.1b',
+    python_requires='>=3.5',
+    description="""Organizes the video files in the correct directories""",
+    long_description=readme(),
+    url='https://github.com/Scheercuzy/video_file_organizer',
+    author='MX',
+    author_email='maxi730@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    # entry_points={
+    #     'console_scripts': [
+    #         'name = name.__main__:main'
+    #     ]
+    # },
+    install_requires=[requirements]
+)
